dfb45fdc795d2cf3b25c0ef75dffb8f59fd4c691
Olga <o.nikolaichenko@ya.ru>
2024-08-14 12:53:44 +0200

    errorSlice for the last error message added

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import booksReduser from './slices/booksSlice';
 import filterReduser from './slices/filterSlice';
+import errorReducer from './slices/errorSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReduser,
     filter: filterReduser,
+    error: errorReducer,
   },
 });
 
