229bd71184b3a15f519e7c5253fc7b2aa70b72b7
Olga <o.nikolaichenko@ya.ru>
2024-08-14 16:41:40 +0200

    isLoading with spinner added

--- api/index.js
+++ api/index.js
@@ -7,13 +7,23 @@ const app = express();
 app.use(cors());
 
 app.get('/random-book', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * booksData.length);
-  const randomBook = booksData[randomIndex];
+  res.json(getRandomBook());
+});
 
-  res.json(randomBook);
+app.get('/random-book-delayed', (req, res) => {
+  setTimeout(() => {
+    res.json(getRandomBook());
+  }, 4000);
 });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
+
+function getRandomBook() {
+  const randomIndex = Math.floor(Math.random() * booksData.length);
+  const randomBook = booksData[randomIndex];
+
+  return randomBook;
+}
